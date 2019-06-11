4c239e587e512c89c16ece0f457d4aa28a60934c
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-11 16:26:42 +0200

    editing api for quote usage

---
+++ src/api/quoteCollection.js
@@ -0,0 +1,19 @@
+let quotesCollection = [
+  {
+    quoteId: 1,
+    quote: "An investment in knowledge always pays the best interest.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quoteId: 2,
+    quote: "Everything has its beauty but not everyone sees it.",
+    author: "Confucius"
+  },
+  {
+    quoteId: 3,
+    quote: "Never trust a computer you can’t throw out a window.",
+    author: "Steve Wozniak"
+  }
+];
+
+export default quotesCollection;
