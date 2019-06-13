193e46c6c33eadfbbafab4677afb739c6013ed59
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-13 22:47:49 +0200

    adding envinronment related quotes

--- src/api/quoteCollection.js
+++ src/api/quoteCollection.js
@@ -1,18 +1,56 @@
 const quotesCollection = [
   {
     quoteId: 1,
-    quote: 'An investment in knowledge always pays the best interest.',
-    author: 'Benjamin Franklin',
+    quote: 'Nature is painting for us, day after day, pictures of infinite beauty.',
+    author: 'John Ruskin',
   },
   {
     quoteId: 2,
-    quote: 'Everything has its beauty but not everyone sees it.',
-    author: 'Confucius',
+    quote: 'What’s the use of a fine house if you haven’t for a tolerable planet to put it on.',
+    author: 'Henry David Thoreau',
   },
   {
     quoteId: 3,
-    quote: 'Never trust a computer you can’t throw out a window.',
-    author: 'Steve Wozniak',
+    quote: 'We won’t have a society if we destroy the environment',
+    author: 'Margaret Mead',
+  },
+  {
+    quoteId: 4,
+    quote: 'The environment is everything that isn’t me',
+    author: 'Albert Einstein',
+  },
+  {
+    quoteId: 5,
+    quote: 'The greatest threat to our planet is the belief that someone else will save it.',
+    author: 'Robert Swan',
+  },
+  {
+    quoteId: 6,
+    quote: 'There is no such thing as ‘away’. When we throw anything away it must go somewhere.',
+    author: 'Annie Leonard',
+  },
+  {
+    quoteId: 7,
+    quote: 'Buy less, choose well.',
+    author: 'Vivienne Westwood',
+  },
+  {
+    quoteId: 8,
+    quote:
+      'Becoming vegan is the most important and direct change we can immediately make to save the planet and its species.',
+    author: 'Chris Hedges',
+  },
+  {
+    quoteId: 9,
+    quote:
+      'The oceans are in trouble; the coasts are in trouble; our marine resources are in trouble. These are not challenges we can sweep aside.',
+    author: 'James Watkins',
+  },
+  {
+    quoteId: 10,
+    quote:
+      'One of the first conditions of happiness is that the link between man and nature shall not be broken.',
+    author: 'Leo Tolstoy',
   },
 ];
 
