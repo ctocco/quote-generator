82a3ad38c22a1c1aab8e9d9f25eb7122684c1497
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-13 22:48:34 +0200

    shuffle helper function for random quotes

---
+++ src/helpers/helperFunctions.js
@@ -0,0 +1,16 @@
+const shuffle = array => {
+  let i = array.length;
+  let j = 0;
+  let temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+export default shuffle;
